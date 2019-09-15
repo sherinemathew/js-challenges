fdc9da41dd0e426b2ea6bbabe59f3f0f5df94172
sherinemathew <sherine.mary.mathew@gmail.com>
2019-09-15 14:08:39 +1000

    challenge done

--- 04_objects.js
+++ 04_objects.js
@@ -81,23 +81,36 @@ const drinkShop = {
 const shop = {
 	shopTitle: () => {
 		// Return the shop title (you have access to the shop object from here)
+		return drinkShop.title;
 	},
 
 	upperCase: string => {
 		// Return an uppercase version of string
+		return string.toUpperCase();
 	},
 
 	upperCaseShopTitle: () => {
 		// Return an uppercase shop title.
 		// Don't repeat yourself. Use the two functions you just built!
+		let title = shop.shopTitle();
+		return shop.upperCase(title);
 	},
 
 	productById: productId => {
 		// Return a specific product object
+		let prodArr = drinkShop.products;
+		for (let product of prodArr){
+			if(product.id === productId){
+				return product;
+			}
+		}
+		return null;
 	},
 
 	productCost: productId => {
 		// given a product id, return its cost. DRY ;)
+		let product = shop.productById(productId);
+		return product.price;
 	},
 
 	formatAddress: email => {
@@ -106,18 +119,59 @@ const shop = {
 		// streetNumber street, city, postcode
 		// E.g. 10 Amelia St, Sydney, 2000
 		// Hint: some destructuring might save you a few lines.
+		let customer = drinkShop.customers[email];
+		if (customer){
+			let {
+			streetNumber,
+			street,
+			city,
+			postcode } = drinkShop.customers[email].address;
+			return `${streetNumber} ${street}, ${city}, ${postcode}`;
+        } else { 
+            return null;
+        }
+	},
+
+	customerOrderById: (email, orderId) => {
+		// Returns the customer order for the provided customer and orderId
+		// Returns null if the order isn't found
+		let customer = drinkShop.customers[email];
+		let orders = customer ? customer.orders : [];
+
+		for (let order of orders) {
+			if (order.id === orderId) {
+				return order;
+			}
+		}
+		return null;
 	},
 
 	totalCost: (email, orderId) => {
 		// Return the total cost of an order.
+		let order = shop.customerOrderById(email, orderId);
+		if (order === null){
+			return 0;
+		}
+		let items = order.items;
+		let total = 0;
+		for (let item of items) {
+			total += shop.productCost(item.productId) * item.qty;
+		}
+		return total;
 	},
 
 	addProduct: (id, title, price) => {
 		// Add a product to the shop.
+		let newProduct = {id: id, title: title, price: price};
+		drinkShop.products.push(newProduct);
+		return true;
 	},
 
 	updateProductPrice: (id, newPrice) => {
 		// Update the price of a specific product
+		let product = shop.productById(id);
+		product.price = newPrice;
+		return true;
 	}
 }
 
